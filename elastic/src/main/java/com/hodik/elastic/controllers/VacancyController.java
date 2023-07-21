07f66b990dac954f6487c1faa9c10f1be5b91da1
m-god <m-godik@ukr.net>
2023-07-21 16:35:45 +0300

    [ELASTIC] Controller tests. VacancyControllerTest + refactoring

--- elastic/src/main/java/com/hodik/elastic/controllers/VacancyController.java
+++ elastic/src/main/java/com/hodik/elastic/controllers/VacancyController.java
@@ -5,7 +5,6 @@ import com.hodik.elastic.dto.VacancyDto;
 import com.hodik.elastic.exceptions.EntityAlreadyExistsException;
 import com.hodik.elastic.exceptions.EntityNotFoundException;
 import com.hodik.elastic.exceptions.VacancyErrorResponse;
-import com.hodik.elastic.mappers.PageableMapper;
 import com.hodik.elastic.mappers.VacancyMapper;
 import com.hodik.elastic.services.EsVacancyService;
 import lombok.extern.log4j.Log4j2;
@@ -23,13 +22,13 @@ import java.util.stream.Collectors;
 public class VacancyController {
     private final EsVacancyService vacancyService;
     private final VacancyMapper vacancyMapper;
-    private final PageableMapper pageableMapper;
+
 
     @Autowired
-    public VacancyController(EsVacancyService vacancyService, VacancyMapper vacancyMapper, PageableMapper pageableMapper) {
+    public VacancyController(EsVacancyService vacancyService, VacancyMapper vacancyMapper) {
         this.vacancyService = vacancyService;
         this.vacancyMapper = vacancyMapper;
-        this.pageableMapper = pageableMapper;
+
     }
 
     @PostMapping()
@@ -51,7 +50,7 @@ public class VacancyController {
     }
 
     @GetMapping("/{id}")
-    public VacancyDto getVacancy(@PathVariable long id) throws EntityAlreadyExistsException {
+    public VacancyDto getVacancy(@PathVariable long id) {
         return vacancyMapper.convertToVacancyDto(vacancyService.findById(id).orElseThrow(EntityNotFoundException::new));
     }
 
@@ -63,11 +62,11 @@ public class VacancyController {
     @PostMapping("/search")
     public List<VacancyDto> searchByCriteria(@RequestBody SearchCriteriaDto searchCriteriaDto) {
         log.info("Search request to index Vacancies" + searchCriteriaDto);
-      return vacancyService.findAllWithFilters(searchCriteriaDto).stream().map(vacancyMapper::convertToVacancyDto).collect(Collectors.toList());
+        return vacancyService.findAllWithFilters(searchCriteriaDto).stream().map(vacancyMapper::convertToVacancyDto).collect(Collectors.toList());
     }
 
     @ExceptionHandler
-    private ResponseEntity<VacancyErrorResponse> exceptionHandler(EntityAlreadyExistsException e) {
+    private ResponseEntity<VacancyErrorResponse> exceptionHandler (EntityAlreadyExistsException e) {
         VacancyErrorResponse message = new VacancyErrorResponse(e.getMessage());
         log.error(e.getMessage());
         return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
@@ -79,6 +78,7 @@ public class VacancyController {
         log.error(e.getMessage());
         return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler
     private ResponseEntity<VacancyErrorResponse> exceptionHandler(IllegalArgumentException e) {
         VacancyErrorResponse message = new VacancyErrorResponse(e.getMessage());
---
+++ elastic/src/test/java/com/hodik/elastic/controllers/VacancyControllerTest.java
@@ -0,0 +1,177 @@
+package com.hodik.elastic.controllers;
+
+import com.hodik.elastic.dto.SearchCriteriaDto;
+import com.hodik.elastic.dto.SearchFilter;
+import com.hodik.elastic.dto.SearchSort;
+import com.hodik.elastic.dto.VacancyDto;
+import com.hodik.elastic.exceptions.EntityAlreadyExistsException;
+import com.hodik.elastic.exceptions.EntityNotFoundException;
+import com.hodik.elastic.mappers.VacancyMapper;
+import com.hodik.elastic.model.Vacancy;
+import com.hodik.elastic.services.EsVacancyService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.hodik.elastic.util.Operations.LIKE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class VacancyControllerTest {
+    public static final long ID = 1L;
+    public static final String ABOUT_PROJECT = "about project";
+    public static final String CREATOR = "Creator";
+    public static final String DESCRIPTION = "Description";
+    public static final String EXPECTED = "Expected";
+    public static final long PROJECT_ID = 1L;
+    public static final String JOB_POSITION = "job position";
+    private final Vacancy expectedVacancy = getVacancyBuild();
+
+    private final List<Vacancy> expectedVacancyList = List.of(expectedVacancy);
+    private final VacancyDto expectedVacancyDto = getVacancyDtoBuild();
+    private final List<VacancyDto> expectedVacancyDtoList = List.of(expectedVacancyDto);
+    private final SearchSort searchSort = new SearchSort("Creator", true);
+    private final List<SearchSort> searchSortList = List.of(searchSort);
+    private final SearchFilter searchFilter = new SearchFilter("Creator", LIKE, List.of("Creator"));
+    private final SearchCriteriaDto searchCriteriaDto = new SearchCriteriaDto(List.of(searchFilter), 0, 2, searchSortList);
+
+
+    @Mock
+    private EsVacancyService vacancyService;
+    @Mock
+    private VacancyMapper vacancyMapper;
+    @InjectMocks
+    private VacancyController vacancyController;
+
+    @Test
+    void createVacancySuccess() throws EntityAlreadyExistsException {
+        //given
+        when(vacancyMapper.convertToVacancy(expectedVacancyDto)).thenReturn(expectedVacancy);
+
+        //when
+        ResponseEntity<HttpStatus> response = vacancyController.createVacancy(expectedVacancyDto);
+        //then
+        verify(vacancyService).create(expectedVacancy);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+    }
+
+    @Test
+    void createVacancyException() throws EntityAlreadyExistsException {
+        //given
+        when(vacancyMapper.convertToVacancy(expectedVacancyDto)).thenReturn(expectedVacancy);
+        Mockito.doThrow(EntityAlreadyExistsException.class).when(vacancyService).create(expectedVacancy);
+        //when
+      assertThrows(EntityAlreadyExistsException.class,
+                () -> vacancyController.createVacancy(expectedVacancyDto));
+        //then
+        verify(vacancyService).create(expectedVacancy);
+
+
+    }
+
+    @Test
+    void updateVacancy() {
+        //given
+        when(vacancyMapper.convertToVacancy(expectedVacancyDto)).thenReturn(expectedVacancy);
+        //when
+        ResponseEntity<HttpStatus> response = vacancyController.updateVacancy(expectedVacancyDto.getId(), expectedVacancyDto);
+        //then
+        verify(vacancyService).update(expectedVacancy.getId(),expectedVacancy);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    void deleteVacancy() {
+        //when
+        ResponseEntity<HttpStatus> response = vacancyController.deleteVacancy(expectedVacancyDto.getId());
+        //then
+        verify(vacancyService).delete(expectedVacancy.getId());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    void getVacancySuccess() {
+        //given
+        when(vacancyService.findById(ID)).thenReturn(Optional.of(expectedVacancy));
+        when(vacancyMapper.convertToVacancyDto(expectedVacancy)).thenReturn(expectedVacancyDto);
+        //when
+        VacancyDto vacancyDto = vacancyController.getVacancy(ID);
+        //then
+        verify(vacancyService).findById(ID);
+        verify(vacancyMapper).convertToVacancyDto(expectedVacancy);
+        assertEquals(expectedVacancyDto, vacancyDto);
+    }
+    @Test
+    void getVacancyException() {
+        //given
+        when(vacancyService.findById(ID)).thenThrow(EntityNotFoundException.class);
+        //when
+        assertThrows(EntityNotFoundException.class, ()-> vacancyController.getVacancy(ID));
+        //then
+        verify(vacancyService).findById(ID);
+    }
+
+    @Test
+    void getVacancies() {
+        //given
+        when(vacancyMapper.convertToVacancyDto(expectedVacancy)).thenReturn(expectedVacancyDto);
+        when(vacancyService.findAll()).thenReturn(expectedVacancyList);
+        //when
+        List<VacancyDto> vacancyDtoList = vacancyController.getVacancies();
+        //then
+        verify(vacancyMapper).convertToVacancyDto(expectedVacancy);
+        verify(vacancyService).findAll();
+        assertEquals(expectedVacancyDtoList, vacancyDtoList);
+
+
+    }
+
+    @Test
+    void searchByCriteria() {
+        //given
+        when(vacancyMapper.convertToVacancyDto(expectedVacancy)).thenReturn(expectedVacancyDto);
+        when(vacancyService.findAllWithFilters(searchCriteriaDto)).thenReturn(expectedVacancyList);
+        //when
+        List<VacancyDto> vacancyDtoList = vacancyController.searchByCriteria(searchCriteriaDto);
+        //then
+        verify(vacancyMapper).convertToVacancyDto(expectedVacancy);
+        verify(vacancyService).findAllWithFilters(searchCriteriaDto);
+        assertEquals(expectedVacancyDtoList, vacancyDtoList);
+    }
+
+    private Vacancy getVacancyBuild() {
+        return Vacancy.builder()
+                .id(ID)
+                .aboutProject(ABOUT_PROJECT)
+                .creator(CREATOR)
+                .description(DESCRIPTION)
+                .expected(EXPECTED)
+                .projectId(PROJECT_ID)
+                .jobPosition(JOB_POSITION)
+                .build();
+    }
+
+    private VacancyDto getVacancyDtoBuild() {
+        return VacancyDto.builder()
+                .id(ID)
+                .aboutProject(ABOUT_PROJECT)
+                .creator(CREATOR)
+                .description(DESCRIPTION)
+                .expected(EXPECTED)
+                .projectId(PROJECT_ID)
+                .jobPosition(JOB_POSITION)
+                .build();
+    }
+}
