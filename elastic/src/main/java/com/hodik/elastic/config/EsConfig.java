fcfaaef8610ab9b8e82f1b5a1b34e2641b4b2c58
Tymofii Hodik <tymofii_hodik@epam.com>
2023-08-15 16:44:52 +0300

    [INFRA] Add client-side  load balancer  between project and elastic services, add prod spring active profile, add scalability for elastic-service

--- elastic/src/main/java/com/hodik/elastic/config/EsConfig.java
+++ elastic/src/main/java/com/hodik/elastic/config/EsConfig.java
@@ -1,19 +1,29 @@
 package com.hodik.elastic.config;
 
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+@Log4j2
 @Configuration
 @EnableElasticsearchRepositories
 public class EsConfig extends ElasticsearchConfiguration {
+    @Value("${elasticsearch.host}")
+    private String elasticHost;
+
+    @Value("${elasticsearch.port}")
+    private Integer elasticPort;
 
     @Override
     public ClientConfiguration clientConfiguration() {
+        log.info("[ELASTIC][CONFIG] Try to establish connection with elastic host={} port={}", elasticHost, elasticPort);
+        String connection = elasticHost + ":" + elasticPort;
         return ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo(connection)
                 .withBasicAuth("elastic", "elastic")
                 .build();
     }
