2690bb7133d238349964dba1d3b5ccd9b58f7299
m-god <m-godik@ukr.net>
2023-08-26 17:21:45 +0300

    [ELASTIC][PROJECTS_APP]- add Converter for different type of DateTime

--- projects_app/src/main/java/org/example/projectapp/mapper/ElasticFilterDtoMapper.java
+++ projects_app/src/main/java/org/example/projectapp/mapper/ElasticFilterDtoMapper.java
@@ -8,6 +8,10 @@ import org.example.projectapp.mapper.dto.ElasticOperation;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+
 @Component
 public class ElasticFilterDtoMapper {
 
@@ -18,14 +22,36 @@ public class ElasticFilterDtoMapper {
     }
 
     public ElasticFilterDto convertToProjectElasticFilterDto(FilterDto filterDto) {
+        FilterDto filterDto1 = convertDataIfNeed(filterDto);
         return ElasticFilterDto.builder()
-                .column(filterDto.getName())
-                .values(filterDto.getValues())
-                .clazz(filterDto.getDataType() == null ?
-                        String.class
-                        : filterDto.getDataType().getClazz())
-                .operation(mapperOperation(filterDto.getOperation()))
+                .column(filterDto1.getName())
+                .clazz(getClazz(filterDto1))
+                .values(filterDto1.getValues())
+                .operation(mapperOperation(filterDto1.getOperation()))
                 .build();
+    }
+
+    private Class<?> getClazz(FilterDto filterDto) {
+        return filterDto.getDataType() == null ?
+                String.class
+                : filterDto.getDataType().getClazz();
+    }
+
+    private FilterDto convertDataIfNeed(FilterDto filterDto) {
+        List<Object> values = filterDto.getValues();
+        if (getClazz(filterDto) == LocalDateTime.class) {
+
+            for (int i = 0; i < values.size(); i++) {
+                if (values.get(i).getClass().isInstance("String")) {
+                    LocalDateTime dateTime = LocalDateTime.parse(values.get(i).toString());
+                    Long epoch = dateTime.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond();
+                    values.set(i, epoch.toString());
+                }
+            }
+        }
+        filterDto.setValues(values);
+
+        return filterDto;
     }
 
     private ElasticOperation mapperOperation(SearchOperation operation) {
