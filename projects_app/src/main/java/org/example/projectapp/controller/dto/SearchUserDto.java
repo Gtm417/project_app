6dfca338d9a0a4aba29f9d2db305c69be2f70913
m-god <m-godik@ukr.net>
2023-09-11 16:34:41 +0300

    [ELASTIC][PROJECTS-APP] refactoring SearchUserDto

--- projects_app/src/main/java/org/example/projectapp/controller/dto/SearchUserDto.java
+++ projects_app/src/main/java/org/example/projectapp/controller/dto/SearchUserDto.java
@@ -2,11 +2,13 @@ package org.example.projectapp.controller.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class SearchUserDto extends SearchDto {
